8c8aeae75012d7ae4b3746edceca43e25db15e3a
Roman-Princ <romanruins08@hotmail.com>
2021-09-02 15:49:04 -0500

    "Added list and assignments command, and post_announcements task"

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import configparser
+import datetime
 import re
 
 import discord
 from discord.ext import commands, tasks
 from canvasapi import Canvas
-
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -71,6 +71,52 @@ async def _assignments(ctx, id):
     embed.description = embed_string
 
     await ctx.send(embed=embed)
+
+
+class_list = [56444, 56967, 56281, 56289, 56709]
+
+class_dict = {
+    "class_list": [
+        56444, 56967, 56281, 56289, 56709
+    ],
+    56444: 0,
+    56967: 0,
+    56281: 0,
+    56289: 0,
+    56709: 0,
+}
+
+
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+
+@tasks.loop(seconds=30)
+async def post_announcements():
+    announcements = canvas.get_announcements(context_codes=class_dict['class_list'], latest_only=True)
+    for announcement in announcements:
+        s = announcement.html_url
+        start = s.find("/courses/") + len("/courses/")
+        end = s.find("/discussion_topics/")
+        course_id = int(s[start:end])
+        if announcement.id != class_dict[course_id]:
+            class_dict[course_id] = announcement.id
+            announcement_course = canvas.get_course(course_id)
+            embed = discord.Embed(
+                title=f"{announcement_course.course_code} Announcement: ({announcement.id})",
+                url=announcement.html_url,
+                description=cleanhtml(announcement.message),
+                timestamp=datetime.datetime.strptime(announcement.posted_at, "%Y-%m-%dT%H:%M:%SZ")
+            )
+            embed.set_footer(text=f"{announcement_course.course_code}")
+
+            channel = await bot.fetch_channel(870019149743669288)
+            await channel.send(embed=embed)
+
+
+post_announcements.start()
 
 if __name__ == '__main__':
     for extension in initial_extensions:
